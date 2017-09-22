2d9ecb31113df37b04a49e2ef5e60463ce03be4d
Alikhan <abakhitbek@gmail.com>
2017-09-22 18:06:02 +0600

    first stage

--- app/src/main/java/kz/kbtu/newsapp/MainActivity.java
+++ app/src/main/java/kz/kbtu/newsapp/MainActivity.java
@@ -1,51 +1,16 @@
 package kz.kbtu.newsapp;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
 public class MainActivity extends AppCompatActivity {
-    private FirebaseAuth mAuth;
-    private FirebaseAuth.AuthStateListener mAuthListener;
-    private FirebaseUser user;
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        mAuth.addAuthStateListener(mAuthListener);
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mAuth = FirebaseAuth.getInstance();
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                user = firebaseAuth.getCurrentUser();
-                if(user != null){
-                    //User is in signed in state and we can go further
-                }
-                else{
-                    //User is in signed out state and we need to open
-                    //the window with authentication screen
-                }
-            }
-        };
+
     }
 
-
-
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        if(mAuthListener != null){
-            mAuth.removeAuthStateListener(mAuthListener);
-        }
-    }
 }
